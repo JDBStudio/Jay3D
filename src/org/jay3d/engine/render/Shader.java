58b53783cacc55be99d76cd8a0e75d01130b4d6d
bucial11 <JuxhinBox@gmail.com>
2014-09-08 00:22:23 +0200

    Finished all of Shader.java class for now

--- src/org/jay3d/engine/render/Shader.java
+++ src/org/jay3d/engine/render/Shader.java
@@ -1,5 +1,11 @@
 package org.jay3d.engine.render;
 
+import org.jay3d.engine.math.Matrix4f;
+import org.jay3d.engine.math.Vector3f;
+import org.jay3d.util.Util;
+
+import java.util.HashMap;
+
 import static org.lwjgl.opengl.GL20.*;
 import static org.lwjgl.opengl.GL32.*;
 /**
@@ -8,9 +14,11 @@ import static org.lwjgl.opengl.GL32.*;
  */
 public class Shader {
     private int program;
+    private HashMap<String, Integer> uniforms;
 
     public Shader(){
         program = glCreateProgram();
+        uniforms = new HashMap<>();
 
         if(program == 0){
             System.err.print("SHADER ERROR: Could not find valid memory location in constructor");
@@ -20,6 +28,17 @@ public class Shader {
 
     public void bind(){
         glUseProgram(program);
+    }
+
+    public void addUniform(String uniformName){
+        int uniformLocation = glGetUniformLocation(program, uniformName);
+        if(uniformLocation == 0xFFFFFFFF){
+            System.err.println("ERROR: Could not find uniform: " + uniformName);
+            new Exception().printStackTrace();
+            System.exit(4);
+        }
+
+        uniforms.put(uniformName, uniformLocation);
     }
 
     public void addVertexShader(String text){
@@ -38,13 +57,13 @@ public class Shader {
         glLinkProgram(program);
         if(glGetProgram(program, GL_LINK_STATUS) == 0){
             System.err.print(glGetShaderInfoLog(program, 1024));
-            System.exit(4);
+            System.exit(3);
         }
         glValidateProgram(program);
 
         if(glGetProgrami(program, GL_VALIDATE_STATUS) == 0){
             System.err.print(glGetProgramInfoLog(program, 1024));
-            System.exit(5);
+            System.exit(3);
         }
     }
 
@@ -61,9 +80,25 @@ public class Shader {
 
         if(glGetShader(shader, GL_COMPILE_STATUS) == 0){
             System.err.print(glGetShaderInfoLog(shader, 1024));
-            System.exit(3);
+            System.exit(2);
         }
 
         glAttachShader(program, shader);
+    }
+
+    public void setUniformi(String uniformName, int value){
+        glUniform1i(uniforms.get(uniformName) ,value);
+    }
+
+    public void setUniformf(String uniformName, float value){
+        glUniform1f(uniforms.get(uniformName), value);
+    }
+
+    public void setUniform(String uniformName, Vector3f value){
+        glUniform3f(uniforms.get(uniformName), value.getX(), value.getY(), value.getZ());
+    }
+
+    public void setUniform(String uniformName, Matrix4f value){
+        glUniformMatrix4(uniforms.get(uniformName), true, Util.createFlippedBuffer(value));
     }
 }
