2a13e26c5f433603f2f30179d485133d4d686520
bucial11 <JuxhinBox@gmail.com>
2014-09-07 18:29:44 +0200

    Added all math functions for 2D vectors

--- src/org/jay3d/engine/Vector2f.java
+++ src/org/jay3d/engine/Vector2f.java
@@ -8,8 +8,65 @@ public class Vector2f {
     private float x, y;
 
     public Vector2f(float x, float y){
-        this.x = y;
+        this.x = x;
         this.y = y;
+    }
+
+    public float length(){
+        return (float)Math.sqrt(x * x + y * y);
+    }
+
+    public float dot(Vector2f v){
+        return x * v.getX() + y * v.getY();
+    }
+
+    public Vector2f normalise(){
+        float length = length();
+
+        x /= length;
+        y /= length;
+
+        return this;
+    }
+
+    public Vector2f rotate(float angle){
+        double radians = Math.toRadians(angle);
+        double cos = Math.cos(radians);
+        double sin = Math.sin(radians);
+
+        return new Vector2f((float)(x * cos - y * sin), (float)(x * sin + y * cos));
+    }
+
+    public Vector2f add(Vector2f v){
+        return new Vector2f(x + v.getX(), y + v.getY());
+    }
+
+    public Vector2f add(float v){
+        return new Vector2f(x + v, y + v);
+    }
+
+    public Vector2f sub(Vector2f v){
+        return new Vector2f(x - v.getX(), y - v.getY());
+    }
+
+    public Vector2f sub(float v){
+        return new Vector2f(x - v, y - v);
+    }
+
+    public Vector2f mul(Vector2f v){
+        return new Vector2f(x * v.getX(), y * v.getY());
+    }
+
+    public Vector2f mul(float v){
+        return new Vector2f(x * v, y * v);
+    }
+
+    public Vector2f div(Vector2f v){
+        return new Vector2f(x / v.getX(), y / v.getY());
+    }
+
+    public Vector2f div(float v){
+        return new Vector2f(x / v, y / v);
     }
 
     @Override
