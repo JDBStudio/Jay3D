482bb0de23c7a13e24d46b674d3c3a7231183dd4
bucial11 <JuxhinBox@gmail.com>
2014-09-08 14:57:35 +0200

    Added Vector3f rotation and added more to Transform#getTransformation

--- src/org/jay3d/engine/render/Transform.java
+++ src/org/jay3d/engine/render/Transform.java
@@ -9,16 +9,22 @@ import org.jay3d.engine.math.Vector3f;
  */
 public class Transform {
     private Vector3f translation;
+    private Vector3f rotation;
 
     public Transform() {
         translation = new Vector3f(0, 0, 0);
+        rotation = new Vector3f(0, 0, 0);
     }
 
     public Matrix4f getTransformation(){
         Matrix4f translationMatrix = new Matrix4f().initTranslation(
                 translation.getX(), translation.getY(), translation.getZ());
+        Matrix4f rotationMatrix = new Matrix4f().initRotation(rotation.getX(),
+                rotation.getY(), rotation.getZ());
 
-        return translationMatrix;
+
+
+        return translationMatrix.mul(rotationMatrix);
     }
 
     public Vector3f getTranslation() {
@@ -31,5 +37,17 @@ public class Transform {
 
     public void setTranslation(float x, float y, float z) {
         this.translation = new Vector3f(x, y, z);
+    }
+
+    public Vector3f getRotation() {
+        return rotation;
+    }
+
+    public void setRotation(Vector3f rotation) {
+        this.rotation = rotation;
+    }
+
+    public void setRotation(float x, float y, float z){
+        this.rotation = new Vector3f(x, y, z);
     }
 }
